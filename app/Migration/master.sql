a75c8959bc8c8ba3504404cc5d82673487762fb0
odan <odan@users.noreply.github.com>
2014-11-16 12:25:35 +0100

    Added migration master sql

---
+++ app/Migration/master.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `role` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `locale` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
---
+++ app/Migration/master_fixture.sql
@@ -0,0 +1,5 @@
+insert into `user` (`id`, `username`, `password`, `enabled`, `role`, `locale`) 
+    values('1','admin','$2y$10$8SCHkI4JUKJ2NA353BTHW.Kgi33HI.2C35xd/j5YUzBx05F1O4lJO','1','ROLE_ADMIN','en_US');
+insert into `user` (`id`, `username`, `password`, `enabled`, `role`, `locale`) 
+    values('2','user','$1$X64.UA0.$kCSxRsj3GKk7Bwy3P6xn1.','1','ROLE_USER','de_DE');
+
