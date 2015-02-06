6e909a37495aee3193c8b9b92662253162fd9a37
odan <odan@users.noreply.github.com>
2015-02-07 00:27:30 +0100

    Added HTML5 shim

--- app/View/Index/html/layout.html.php
+++ app/View/Index/html/layout.html.php
@@ -11,6 +11,11 @@
         <?php echo $this->block('css'); ?>
         <script type="text/javascript" src="assets/js/jquery.min.js"></script>
         <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
         <?php echo $this->block('js'); ?>
     </head>
     <body>
