6300c0e7d0223fd535f7bbcc8483f4fd01a5b02d
odan <odan@users.noreply.github.com>
2015-03-15 19:18:55 +0100

    Added schema_version to master schema

--- app/Schema/001_master.sql
+++ app/Schema/001_master.sql
@@ -1,3 +1,21 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+/* ---------------------------------------------------------------------------*/
+
+CREATE TABLE `schema_version` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_user_id` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_created_user_id` (`created_user_id`),
+  KEY `idx_updated_user_id` (`updated_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -7,3 +25,11 @@ CREATE TABLE `user` (
   `locale` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+
+INSERT `schema_version` (`version`, `created_at`) VALUES ('1', NOW());
+
+/* ---------------------------------------------------------------------------*/
+
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
--- app/Schema/002_master_fixture.sql
+++ app/Schema/002_master_fixture.sql
@@ -1,5 +1,19 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+/* ---------------------------------------------------------------------------*/
+
 insert into `user` (`id`, `username`, `password`, `enabled`, `role`, `locale`) 
     values('1','admin','$2y$10$8SCHkI4JUKJ2NA353BTHW.Kgi33HI.2C35xd/j5YUzBx05F1O4lJO','1','ROLE_ADMIN','en_US');
 insert into `user` (`id`, `username`, `password`, `enabled`, `role`, `locale`) 
     values('2','user','$1$X64.UA0.$kCSxRsj3GKk7Bwy3P6xn1.','1','ROLE_USER','de_DE');
 
+
+UPDATE `schema_version` SET `version` = '2', `updated_at` = NOW() WHERE `id` = '1'; 
+
+/* ---------------------------------------------------------------------------*/
+
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
