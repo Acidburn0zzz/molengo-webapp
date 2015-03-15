f2259beb70b84b95dafab0a53e3b35207ca860c7
odan <odan@users.noreply.github.com>
2015-03-15 18:06:41 +0100

    Set fixed navbar as super basic template with a fixed top navbar along with some additional content.

--- app/View/Index/html/index.html.php
+++ app/View/Index/html/index.html.php
@@ -1,9 +1,10 @@
 <div class="jumbotron">
-    <h1><?php wh(__('WebApp')); ?></h1>
-    <p class="lead"><?php wh(__('Welcome')); ?></p>
+    <h1><?php wh(__('WebApp example')); ?></h1>
+    <p>This example is a quick exercise to illustrate how the default,
+        static and fixed to top navbar work. It includes the responsive CSS
+        and HTML, so it also adapts to your viewport and device.
+    </p>
+    <p>
+        <a class="btn btn-lg btn-primary" href="#" role="button">Example button</a>
+    </p>
 </div>
-
-<div class="bgimg"></div>
-
-<br>
-
