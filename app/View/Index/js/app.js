3ce6c74071bb88a66dd6744751f98e0dfd1cd814
odan <odan@users.noreply.github.com>
2015-03-27 00:05:55 +0100

    Fixed bs-modal window scrollbar

--- app/View/Index/js/app.js
+++ app/View/Index/js/app.js
@@ -4,4 +4,6 @@ app.page = {};
 /**
  * Fix for open modal is shifting body content to the left #9855
  */
-//$.fn.modal.Constructor.prototype.setScrollbar = function() {};
+$.fn.modal.Constructor.prototype.setScrollbar = function() {
+};
+
